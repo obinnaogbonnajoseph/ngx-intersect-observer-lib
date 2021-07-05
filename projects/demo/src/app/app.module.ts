41f395ed89cfbfcb488fa25d41fa8db146b966c3
Obinna Ogbonna <oogbonna@gidimobile.com>
2021-07-05 13:48:04 +0100

    add implementation to demo app

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxIntersectObserverModule } from 'projects/ngx-intersect-observer/src/public-api';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxIntersectObserverModule
   ],
   providers: [],
   bootstrap: [AppComponent]
